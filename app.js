157db4331f13d82934297626f268beb6ef3f3e2e
venemousCoder <ahmadgameriv@gmail.com>
2025-05-26 00:16:57 +0100

    fix oauth finally

--- app.js
+++ app.js
@@ -45,13 +45,15 @@ store.on("error", function (error) {
   throw new Error("Session store error: " + error);
 });
 
+app.set("trust proxy", 1);
+
 app.use(
   session({
     store: store,
     cookie: {
       maxAge: 24 * 60 * 60 * 1000,
       httpOnly: true, // Ensures cookies are sent only over HTTP(S), not client JS
-      secure: process.env.NODE_ENV === "development", // Ensures cookies are sent only over HTTPS
+      secure: process.env.NODE_ENV === "production", // Ensures cookies are sent only over HTTPS
       sameSite: "lax", // Prevents CSRF attacks
     },
     resave: false,
